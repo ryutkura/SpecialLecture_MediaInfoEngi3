0e7fadbfd13ec73a09dde7875a225e52957d9a76
ryutkura <ryutkura@gmail.com>
2023-10-04 15:53:58 +0900

    3つバージョンを書きました

--- Floyd_allserch.java
+++ Floyd_allserch.java
@@ -1,39 +1,37 @@
 class Floyd_allserch{
-    public static double calcflo(int[] Flv){
+    public static double calcflo(int[] Flv, int N){
         double A = 0;
         double B = 0;
         double ab = 1;
-        for(int i=0;i<3;i++){
+        for(int i=0;i<N;i++){
             if(Flv[i] == 1){
                 A = A + Math.sqrt(i+1);
-                System.out.println("Aの合計値は"+A+"です。");
             }
             else{
                 B = B + Math.sqrt(i+1);
-                System.out.println("Bの合計値は"+B+"です。");
             }
         }
         ab = A - B;
         return Math.abs(ab);
-        // System.out.println("差分の絶対値は"+Math.abs(ab)+"です。");
     }
 
     public static void main(String args[]){
-        int[] Flv = new int[3];
-        //Flv = new int[]{0,1,1,0,0};
+        int N = 3;
+        int[] Flv = new int[N];
+
+        long startTime = System.nanoTime();
         for(int i=0;i<2;i++){
             Flv[0] = i;
-            // System.out.println("Flv["+i+"]は"+Flv[i]+"です。");
             for(int j=0;j<2;j++){
                 Flv[1] = j;
-                // System.out.println("Flv["+j+"]は"+Flv[j]+"です。");
                 for(int k=0;k<2;k++){
                     Flv[2] = k;
-                    // System.out.println("Flv["+k+"]は"+Flv[k]+"です。");
-                    System.out.println("Floyd問題の今回の回答は"+calcflo(Flv)+"です。");
+                    System.out.println("Floyd問題の今回の回答は"+calcflo(Flv, N)+"です。");
                 }
             }
         }
+        long endTime = System.nanoTime();
+        System.out.println("処理時間：" + (endTime - startTime) + " ナノ秒");
 
     }
 }
