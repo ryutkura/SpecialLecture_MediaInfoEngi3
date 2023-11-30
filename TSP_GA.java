1b380ae69e60ded3435497a7ca95481fdd0e980d
ryutkura <ryutakakura1021@gmail.com>
2023-11-30 15:44:19 +0900

    都市データを追加しました

--- TSP_GA.java
+++ TSP_GA.java
@@ -1,9 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
 public class TSP_GA {
+
+    private static ArrayList<City> readCities(String filePath) {
+        ArrayList<City> cities = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] coordinates = line.split("\\s+");
+                double x = Double.parseDouble(coordinates[0]);
+                double y = Double.parseDouble(coordinates[1]);
+                cities.add(new City(x, y));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cities;
+    }
+
     public static void main(String[] args){
+
+        // 新しい都市データの読み込み
+        ArrayList<City> cities = readCities("WesternSaharaPlot.txt");
+
+        // ... (既存のコード)
+
+        // 初期個体群を生成し、新しい都市データを使用して進化を開始
+        // Population population = new Population(POPULATION_SIZE, cities);
+
+   
 
     }
 }
@@ -12,7 +44,7 @@ class City{
     private double x;
     private double y;
 
-    public City(int x, int y) {
+    public City(double x, double y) {
         this.x = x;
         this.y = y;
     }
@@ -30,6 +62,13 @@ class City{
         double dx = this.x - otherCity.x;
         double dy = this.y - otherCity.y;
         return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public static City createCity(String coordinates) {
+        String[] coords = coordinates.split("\\s+");
+        double x = Double.parseDouble(coords[0]);
+        double y = Double.parseDouble(coords[1]);
+        return new City(x, y);
     }
 }
 
