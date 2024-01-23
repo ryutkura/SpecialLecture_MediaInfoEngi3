fa466b551dcc5c0196ee8f3c2cd7a0685eec41ed
ryutkura <ryutakakura1021@gmail.com>
2024-01-23 16:10:48 +0900

    評価関数の実装。点数付けはまだ

--- MusicCompositionAndMidiCreator.java
+++ MusicCompositionAndMidiCreator.java
@@ -72,6 +72,75 @@ public class MusicCompositionAndMidiCreator {
         }
     }
 
+    // コードへの適合度を評価するメソッド
+    public static int evaluateChordConformity(int[] melody, int[] chordNotes) {
+        int conformingNotes = 0;
+        for (int note : melody) {
+            for (int chordNote : chordNotes) {
+                if (note == chordNote) {
+                    conformingNotes++;
+                    break;
+                }
+            }
+        }
+        return conformingNotes;
+    }
+
+    // メロディーラインの最終音の音高を評価するメソッド
+    public static int evaluateMelodyFinalNote(int[] melody, int[] chordNotes) {
+        int lastNote = melody[melody.length - 1];
+        for (int chordNote : chordNotes) {
+            if (lastNote == chordNote) {
+                return 1; // 高評価
+            }
+        }
+        return -1; // 低評価
+    }
+
+    // メロディーラインのつながりを評価するメソッド
+    public static int evaluateMelodyConnection(int[] melody) {
+        int score = 0;
+        for (int i = 0; i < melody.length - 1; i++) {
+            int diff = Math.abs(melody[i] - melody[i + 1]);
+            if (diff <= 2) {
+                score += 1; // 連続している場合に加点
+            } else if (diff > 7) {
+                score -= 5; // 不自然な変化に減点
+            }
+        }
+        return score;
+    }
+
+    // 経過音の有無を評価するメソッド
+    public static int evaluatePassingTone(int[] melody) {
+        int score = 0;
+        // 評価ロジックを実装
+        return score;
+    }
+
+    // 刺繍音の有無を評価するメソッド
+    public static int evaluateOrnamentalTone(int[] melody) {
+        int score = 0;
+        // 評価ロジックを実装
+        return score;
+    }
+
+    // 強拍でのコード内音を評価するメソッド
+    public static int evaluateStrongBeatConformity(int[] melody, int[] chordNotes, int[] strongBeats) {
+        int score = 0;
+        for (int beat : strongBeats) {
+            if (beat < melody.length) {
+                for (int chordNote : chordNotes) {
+                    if (melody[beat] == chordNote) {
+                        score += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return score;
+    }
+    
     private static int[][][] generateInitialPopulation(int num_ind, int num_code, int num_measure) {
         int[][][] population = new int[num_ind][num_code][num_measure];
         Random random = new Random();
